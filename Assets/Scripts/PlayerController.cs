5fa14a1ff470cd17f6b80d349d9ca2a5894be205
Fazekas Milán <fazekas.milan.20f@szlgbp.hu>
2022-05-18 18:48:14 +0200

    wall detection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,34 +6,95 @@ public class PlayerController : MonoBehaviour
 {
     public int speed = 5;
     public Animator animator;
-    SpriteRenderer sr;
+    public SpriteRenderer sr;
+    public Vector2 moveDir;
+    public Animator anim;
+    public GameObject cam;
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        moveDir = new Vector2(1, 0);
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
+        float interpolation = speed * 1.5f * Time.deltaTime;
+        cam.transform.position = new Vector3(Mathf.Lerp(cam.transform.position.x, transform.position.x, interpolation),
+                                                Mathf.Lerp(cam.transform.position.y, transform.position.y, interpolation),
+                                                -10);
         animator.StartPlayback();
-        if (Input.GetKey(KeyCode.W))
+        var bottomRightDetectionPoint = transform.position - new Vector3(-0.35f, transform.localScale.x / 6.2f, 1);
+        var bottomLeftDetectionPoint = transform.position - new Vector3(0.35f, transform.localScale.x / 6.2f, 1);
+        RaycastHit2D bottomRightRay = Physics2D.Raycast(bottomRightDetectionPoint, moveDir, speed * Time.deltaTime);
+        RaycastHit2D bottomLeftRay = Physics2D.Raycast(bottomLeftDetectionPoint, moveDir, speed * Time.deltaTime);
+        RaycastHit2D middleRay = Physics2D.Raycast(transform.position - new Vector3(0, 0.5f, 0), moveDir, speed * Time.deltaTime);
+        if (bottomRightRay.collider == null && bottomLeftRay.collider == null && middleRay.collider == null)
         {
-            transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+            if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S))
+            {
+                moveDir = new Vector2(-0.7f, -0.7f);
+                sr.flipX = true;
+                animator.StopPlayback();
+                Move();
+            }
+            else if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
+            {
+                moveDir = new Vector2(-0.7f, 0.7f);
+                sr.flipX = true;
+                animator.StopPlayback();
+                Move();
+            }
+            else if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.S))
+            {
+                moveDir = new Vector2(0.7f, -0.7f);
+                sr.flipX = false;
+                animator.StopPlayback();
+                Move();
+            }
+            else if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W))
+            {
+                moveDir = new Vector2(0.7f, 0.7f);
+                sr.flipX = false;
+                animator.StopPlayback();
+                Move();
+            }
+            else if (Input.GetKey(KeyCode.W))
+            {
+                moveDir = new Vector2(0, 1);
+                Move();
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                moveDir = new Vector2(0, -1);
+                Move();
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                moveDir = new Vector2(1, 0);
+                sr.flipX = false;
+                animator.StopPlayback();
+                Move();
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                moveDir = new Vector2(-1, 0);
+                sr.flipX = true;
+                animator.StopPlayback();
+                Move();
+            }
+            
+            
         }
-        if (Input.GetKey(KeyCode.S))
+        else
         {
-            transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
+            transform.position -= new Vector3(moveDir.x, moveDir.y, 0) / 1000;
         }
-        if (Input.GetKey(KeyCode.D))
-        {
-            sr.flipX = false;
-            animator.StopPlayback();
-            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            sr.flipX = true;
-            animator.StopPlayback();
-            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
-        }
+    }
+
+
+    void Move()
+    {
+        transform.position += new Vector3(moveDir.x, moveDir.y, 0) * speed * Time.deltaTime;
     }
 }
