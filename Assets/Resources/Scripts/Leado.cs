a246bf6c311d743d02cfef6090851b08b5c64a09
Fazekas Milán <fazekas.milan.20f@szlgbp.hu>
2022-05-25 14:57:13 +0200

    add delivery

--- Assets/Resources/Scripts/Leado.cs
+++ Assets/Resources/Scripts/Leado.cs
@@ -10,19 +10,20 @@ public class Leado : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void OnTriggerStay2D(Collider2D col)
     {
         if (transform.GetChild(0).transform.childCount is 1)
         {
             item = transform.GetChild(0).transform.GetChild(0).gameObject;
         }
-        else item = null;
+        else
+        {
+            item = null;
+        }
 
 
         if (item is not null && item.tag is "Soup")
         {
-            Debug.Log("leado");
             item.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Images/plate_dirty");
             item.tag = "Dirty";
         }
