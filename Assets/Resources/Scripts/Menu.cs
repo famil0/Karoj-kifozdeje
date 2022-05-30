0e3d20e51cac8d721d4484b9c572c91a54a82b36
Fazekas Milán <fazekas.milan.20f@szlgbp.hu>
2022-05-30 18:04:01 +0200

    change resolution

--- Assets/Resources/Scripts/Menu.cs
+++ Assets/Resources/Scripts/Menu.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
 using UnityEngine.UI;
+using TMPro;
 
 public class Menu : MonoBehaviour
 {
-    public GameObject settings;
+    public GameObject settingsMenu;
+    public GameObject screenResolution;
     public List<GameObject> buttons = new List<GameObject>();
+    public float t = 0.7f;
+    public Dictionary<string, int> resolutions = new Dictionary<string, int>()
+    {
+        { "1920x1080", 0 }, { "1280x720", 1 }, { "800x600", 2 }
+    };
+
+
 
     private void Start()
     {
-        settings.SetActive(false);
-        settings.GetComponent<RectTransform>().transform.localScale = Vector3.zero;
+        settingsMenu.SetActive(false);
+        settingsMenu.GetComponent<RectTransform>().transform.localScale = Vector3.zero;
+
+        foreach (var res in resolutions)
+        {
+            screenResolution.GetComponent<TMP_Dropdown>().options.Add(new TMP_Dropdown.OptionData(text: res.Key));
+        }
+        screenResolution.transform.Find("Label").GetComponent<TMP_Text>().text = resolutions.ElementAt(0).Key;
     }
     public void Play()
     {
@@ -26,9 +42,8 @@ public class Menu : MonoBehaviour
         {
             button.GetComponent<Button>().interactable = false;
         }
-        float t = 0.7f;
         GetComponent<RectTransform>().transform.DOLocalMoveX(-800, t);
-        settings.GetComponent<RectTransform>().transform.DOScale(Vector3.one, t);
+        settingsMenu.GetComponent<RectTransform>().transform.DOScale(Vector3.one, t);
     }
 
     public void SettingsHide()
@@ -37,13 +52,19 @@ public class Menu : MonoBehaviour
         {
             button.GetComponent<Button>().interactable = true;
         }
-        float t = 0.7f;
         GetComponent<RectTransform>().transform.DOLocalMoveX(0, t);
-        settings.GetComponent<RectTransform>().transform.DOScale(Vector3.zero, t);
+        settingsMenu.GetComponent<RectTransform>().transform.DOScale(Vector3.zero, t);
     }
 
     public void Exit()
     {
         Application.Quit();
+    }
+
+    public void SetScreen()
+    {
+        int width = int.Parse(resolutions.ElementAt(screenResolution.GetComponent<TMP_Dropdown>().value).Key.Split("x")[0]);
+        int height = int.Parse(resolutions.ElementAt(screenResolution.GetComponent<TMP_Dropdown>().value).Key.Split("x")[1]);
+        Screen.SetResolution(width, height, true);
     }
 }
